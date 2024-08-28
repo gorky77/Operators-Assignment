9f77c9e9a5c93d6750cf9807e55f930c8f1e54b6
Gorkem Okan <167126305+gorky77@users.noreply.github.com>
2024-08-28 08:53:49 -0400

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        // Properties of the Employee class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloading the == operator to compare Employee objects by Id
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            // If both are null, or both are the same instance, return true
+            if (Object.ReferenceEquals(emp1, emp2))
+            {
+                return true;
+            }
+
+            // If one is null, but not both, return false
+            if (((object)emp1 == null) || ((object)emp2 == null))
+            {
+                return false;
+            }
+
+            // Return true if the fields match
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overloading the != operator to compare Employee objects by Id
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        // Override Equals() to satisfy compiler warnings
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Employee emp = (Employee)obj;
+            return Id == emp.Id;
+        }
+
+        // Override GetHashCode() to satisfy compiler warnings
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate and initialize two Employee objects
+            Employee emp1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" };
+
+            // Compare the two Employee objects using the overloaded == operator
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("The Employees are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The Employees are different.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
